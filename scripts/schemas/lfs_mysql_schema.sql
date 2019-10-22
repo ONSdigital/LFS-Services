a638b5f903f1cfc94680447e7b031fdbe267e36b
Paul Soule <22938608+pds2208@users.noreply.github.com>
2019-10-22 09:10:45 +0100

    Address Upload - refactoring

--- scripts/schemas/lfs_mysql_schema.sql
+++ scripts/schemas/lfs_mysql_schema.sql
@@ -144,5 +144,5 @@ CREATE TABLE `addresses`
 -- Records of users
 -- ----------------------------
 
-nsert into users(username, password)
+insert into users(username, password)
 values ('Admin', '$2a$04$Su7c9o6E9pLaGut2Nv9FqO2ZUbntDmUweOlO/Vj3hczi86qrnbKK2');
