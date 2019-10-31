7ada7d8784a67cab82ecb31e8247ed6be7b1548b
elthorne <elinor.thorne@ons.gov.uk>
2019-10-31 09:05:37 +0000

    Fixed annual batch ID

--- api/batch_service.go
+++ api/batch_service.go
@@ -80,7 +80,7 @@ func (b BatchHandler) generateQuarterBatchId(quarter int, year int, description 
 func (b BatchHandler) generateYearBatchId(year int, description string) error {
 	// Set batch variables
 	batch := types.AnnualBatch{
-		Id:          0,
+		Id:          1,
 		Year:        year,
 		Status:      0,
 		Description: description,
