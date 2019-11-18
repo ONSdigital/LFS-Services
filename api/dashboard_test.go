2e7704f9ae9994691680332fc0d7b9dbba35dc57
elthorne <elinor.thorne@ons.gov.uk>
2019-11-18 15:44:01 +0000

    WIP

--- api/dashboard_test.go
+++ api/dashboard_test.go
@@ -4,16 +4,53 @@ import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
+	"services/config"
 	"testing"
 )
 
+var batchTable string
+
+//var gbBatchTable string
+//var niBatchTable string
+var quarterlyBatchTable string
+var annualBatchTable string
+
+func init() {
+	batchTable = config.Config.Database.MonthlyBatchTable
+	if batchTable == "" {
+		panic("monthly batch table configuration not set")
+	}
+
+	// TODO: Ask Matthew - does the dashboard need to display GB/NI or just monthly?
+	//gbBatchTable = config.Config.Database.GbBatchTable
+	//if gbBatchTable == "" {
+	//	panic("gb batch table configuration not set")
+	//}
+	//
+	//niBatchTable = config.Config.Database.NiBatchTable
+	//if niBatchTable == "" {
+	//	panic("ni batch table configuration not set")
+	//}
+
+	quarterlyBatchTable = config.Config.Database.QuarterlyBatchTable
+	if quarterlyBatchTable == "" {
+		panic("quarterly batch table configuration not set")
+	}
+
+	annualBatchTable = config.Config.Database.AnnualBatchTable
+	if annualBatchTable == "" {
+		panic("annual batch table configuration not set")
+	}
+}
+
 func TestDashboardSuccess1(t *testing.T) {
 	var tc = testCase{
-		expectedCode:  200,
-		expectedItems: 34, // TODO: Count db and assign here/there
+		expectedCode: 200,
 	}
 
 	tearDown(t)
+
+	// Set up random amount of batches
 	setupMonthlyTables(t, 1, 12, 2014, 4)
 	setupMonthlyTables(t, 1, 7, 2015, 4)
 	setupMonthlyTables(t, 8, 5, 2015, 0)
@@ -22,6 +59,78 @@ func TestDashboardSuccess1(t *testing.T) {
 	setupQuarterlyTables(t, 1, 2, 2015, 4)
 	setupQuarterlyTables(t, 3, 2, 2015, 0)
 	setupAnnualTables(t, 2014, 4)
+
+	monthlyTotal := countRows(t, batchTable)
+	quarterlyTotal := countRows(t, quarterlyBatchTable)
+	annualTotal := countRows(t, annualBatchTable)
+
+	expectedTotal := monthlyTotal + quarterlyTotal + annualTotal
+	tc.expectedItems = expectedTotal
+
+	assertDashboardTest(t, &tc)
+}
+
+func TestDashboardSuccess2(t *testing.T) {
+	var tc = testCase{
+		expectedCode: 200,
+	}
+
+	tearDown(t)
+
+	// Set up random amount of batches
+	setupMonthlyTables(t, 1, 12, 2014, 4)
+	setupQuarterlyTables(t, 1, 4, 2014, 4)
+	setupQuarterlyTables(t, 1, 2, 2015, 4)
+
+	monthlyTotal := countRows(t, batchTable)
+	quarterlyTotal := countRows(t, quarterlyBatchTable)
+	annualTotal := countRows(t, annualBatchTable)
+
+	expectedTotal := monthlyTotal + quarterlyTotal + annualTotal
+	tc.expectedItems = expectedTotal
+
+	assertDashboardTest(t, &tc)
+}
+
+func TestDashboardSuccess3(t *testing.T) {
+	var tc = testCase{
+		expectedCode: 200,
+	}
+
+	tearDown(t)
+
+	// Set up random amount of batches
+	setupMonthlyTables(t, 1, 12, 2014, 4)
+	setupQuarterlyTables(t, 1, 4, 2014, 4)
+	setupAnnualTables(t, 2014, 4)
+
+	monthlyTotal := countRows(t, batchTable)
+	quarterlyTotal := countRows(t, quarterlyBatchTable)
+	annualTotal := countRows(t, annualBatchTable)
+
+	expectedTotal := monthlyTotal + quarterlyTotal + annualTotal
+	tc.expectedItems = expectedTotal
+
+	assertDashboardTest(t, &tc)
+}
+
+func TestDashboardSuccess4(t *testing.T) {
+	var tc = testCase{
+		expectedCode: 200,
+	}
+
+	tearDown(t)
+
+	// Set up random amount of batches
+	setupMonthlyTables(t, 1, 3, 2014, 0)
+
+	monthlyTotal := countRows(t, batchTable)
+	quarterlyTotal := countRows(t, quarterlyBatchTable)
+	annualTotal := countRows(t, annualBatchTable)
+
+	expectedTotal := monthlyTotal + quarterlyTotal + annualTotal
+	tc.expectedItems = expectedTotal
+
 	assertDashboardTest(t, &tc)
 }
 
@@ -37,10 +146,11 @@ func assertDashboardTest(t *testing.T, tc *testCase) {
 
 	// Assert number of batches returned in JSON
 	if w.Code == 200 {
-		if !assert.Equal(t, bytes.Count(w.Body.Bytes(), []byte("{")), tc.expectedItems) {
+		actual := bytes.Count(w.Body.Bytes(), []byte("{"))
+		if !assert.Equal(t, actual, tc.expectedItems) {
 			t.Fatalf("\n\n>>>>> Expected %v batches. Actual number of retrieved batches were %v",
 				tc.expectedItems,
-				bytes.Count(w.Body.Bytes(), []byte("{")))
+				actual)
 		}
 	}
 
